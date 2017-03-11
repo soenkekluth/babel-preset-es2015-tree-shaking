4bfa8cc94df163e9495f5a6dde4f68a84e9bd249
Sönke Kluth <mail@soenkekluth.com>
2017-03-11 23:46:10 +0100

    use require.resolve

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-
 module.exports = {
   plugins: [
-    require('babel-plugin-transform-decorators-legacy'),
-    require('babel-plugin-transform-class-properties'),
-    require('babel-plugin-syntax-dynamic-import'),
-    [require("babel-plugin-transform-es2015-template-literals"),{ loose: true }],
-    require("babel-plugin-transform-es2015-literals"),
-    require("babel-plugin-transform-es2015-function-name"),
-    require("babel-plugin-transform-es2015-arrow-functions"),
-    require("babel-plugin-transform-es2015-block-scoped-functions"),
-    [require("babel-plugin-transform-es2015-classes"), { loose: true }],
-    require("babel-plugin-transform-es2015-object-super"),
-    require("babel-plugin-transform-es2015-shorthand-properties"),
-    [require("babel-plugin-transform-es2015-computed-properties"), { loose: true }],
-    [require("babel-plugin-transform-es2015-for-of"), { loose: true }],
-    require("babel-plugin-transform-es2015-sticky-regex"),
-    require("babel-plugin-transform-es2015-unicode-regex"),
-    require("babel-plugin-check-es2015-constants"),
-    [require("babel-plugin-transform-es2015-spread"), { loose: true }],
-    require("babel-plugin-transform-es2015-parameters"),
-    [require("babel-plugin-transform-es2015-destructuring"), { loose: true }],
-    require("babel-plugin-transform-es2015-block-scoping"),
-    require("babel-plugin-transform-es2015-typeof-symbol"),
-    require("babel-plugin-transform-object-rest-spread"),
-    [require("babel-plugin-transform-regenerator"), { async: false, asyncGenerators: false }],
+    require.resolve('babel-plugin-transform-decorators-legacy'),
+    require.resolve('babel-plugin-transform-class-properties'),
+    require.resolve('babel-plugin-syntax-dynamic-import'),
+    [
+      require.resolve('babel-plugin-transform-es2015-template-literals'),
+      { loose: true }
+    ],
+    require.resolve('babel-plugin-transform-es2015-literals'),
+    require.resolve('babel-plugin-transform-es2015-function-name'),
+    require.resolve('babel-plugin-transform-es2015-arrow-functions'),
+    require.resolve('babel-plugin-transform-es2015-block-scoped-functions'),
+    [require.resolve('babel-plugin-transform-es2015-classes'), { loose: true }],
+    require.resolve('babel-plugin-transform-es2015-object-super'),
+    require.resolve('babel-plugin-transform-es2015-shorthand-properties'),
+    [
+      require.resolve('babel-plugin-transform-es2015-computed-properties'),
+      { loose: true }
+    ],
+    [require.resolve('babel-plugin-transform-es2015-for-of'), { loose: true }],
+    require.resolve('babel-plugin-transform-es2015-sticky-regex'),
+    require.resolve('babel-plugin-transform-es2015-unicode-regex'),
+    require.resolve('babel-plugin-check-es2015-constants'),
+    [require.resolve('babel-plugin-transform-es2015-spread'), { loose: true }],
+    require.resolve('babel-plugin-transform-es2015-parameters'),
+    [require.resolve('babel-plugin-transform-es2015-destructuring'), { loose: true }],
+    require.resolve('babel-plugin-transform-es2015-block-scoping'),
+    require.resolve('babel-plugin-transform-es2015-typeof-symbol'),
+    require.resolve('babel-plugin-transform-object-rest-spread'),
+    [
+      require.resolve('babel-plugin-transform-regenerator'),
+      { async: false, asyncGenerators: false }
+    ]
   ]
 };
